d300f260012033513614a68d8cd48636bf045254
Dylan Oh <dylanoh@Dylans-MacBook-Pro.local>
2022-09-05 20:04:35 +0800

    Added restaurant card component placeholder

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://media.istockphoto.com/photos/restaurant-picture-id146725693?k=20&m=146725693&s=612x612&w=0&h=GoJe_uuXVddKC9dkLolLqui3CcIwDnzy8QvjLwlKmhs=",
+    ],
+    address = "100 Some Random Street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Text style={styles.title}>{name}</Text>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+  },
+  cover: {
+    padding: 20,
+    backgroundColor: "white",
+  },
+  title: {
+    padding: 16,
+  },
+});
